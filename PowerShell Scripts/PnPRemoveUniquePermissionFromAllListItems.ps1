6d6a2dc0be3544a855b42f7645abe11116736217
Sanjay Kumar Pathak <sanjay0778@gmail.com>
2022-06-07 11:45:14 +0530

    PnP PowerShell to remove unique permissions
    PnP PowerShell script to remove unique permissions from all folders and files in a SharePoint list or library.

---
+++ PowerShell Scripts/PnPRemoveUniquePermissionFromAllListItems.ps1
@@ -0,0 +1,29 @@
+﻿#This PnP PowerShell script removes unique permissions from all the folders and files for a given SharePoint list/ library
+#Parameters
+$SiteURL = "https://abc.sharepoint.com/sites/forms1"
+$ListName = "TestDocLibrary"
+#$ListName = "Documents"
+
+
+Write-Host $SiteURL
+Write-Host $FolderServerRelativeURL 
+#Connect to the site
+Connect-PnPOnline -Url $SiteURL -Interactive
+ 
+#Get all list items in batches
+$ListItems = Get-PnPListItem -List $ListName -PageSize 500
+ 
+#Iterate through each list item
+ForEach($ListItem in $ListItems)
+{
+    #Check if the Item has unique permissions
+    $HasUniquePermissions = Get-PnPProperty -ClientObject $ListItem -Property "HasUniqueRoleAssignments"
+    If($HasUniquePermissions)
+    {       
+        $Msg = "Deleting Unique Permissions on {0} '{1}' at {2} " -f $ListItem.FileSystemObjectType,$ListItem.FieldValues["FileLeafRef"],$ListItem.FieldValues["FileRef"]
+        Write-host $Msg
+        #Delete unique permissions on the list item
+        Set-PnPListItemPermission -List $ListName -Identity $ListItem.ID -InheritPermissions
+    }
+}
+
